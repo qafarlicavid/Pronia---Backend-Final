406593a8171123bc36393bb9737a21dce21c8d6c
qafarlicavid <javidig@code.edu.az>
2023-02-02 15:17:25 +0400

    Common Folder created

---
+++ Pronia Final - Backend/Pronia Site - Backend/Database/Models/Common/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Pronia_Site___Backend.Database.Models.Common
+{
+    public abstract class BaseEntity<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+}
---
+++ Pronia Final - Backend/Pronia Site - Backend/Database/Models/Common/IAuditable.cs
@@ -0,0 +1,8 @@
+﻿namespace Pronia_Site___Backend.Database.Models.Common
+{
+    public interface IAuditable
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
