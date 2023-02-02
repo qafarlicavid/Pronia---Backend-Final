1c6c32e36cee509520e4ac4ab6fcd0bee4aba2f4
qafarlicavid <javidig@code.edu.az>
2023-02-02 16:23:58 +0400

    EmailService and SMTPService created

---
+++ Pronia Final - Backend/Pronia Site - Backend/Services/Abstracts/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using Pronia_Site___Backend.Contracts.Email;
+
+namespace Pronia_Site___Backend.Services.Abstracts
+{
+    public interface IEmailService
+    {
+        public void Send(MessageDto messageDto);
+    }
+}
