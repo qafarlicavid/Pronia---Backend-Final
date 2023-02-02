49847e98eb7a80870b177a1e76d32cd386a945bd
qafarlicavid <javidig@code.edu.az>
2023-02-02 16:13:39 +0400

    Contact Created

---
+++ Pronia Final - Backend/Pronia Site - Backend/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pronia_Site___Backend.Areas.Admin.ViewModels.Contact;
+using Pronia_Site___Backend.Database;
+
+namespace Pronia_Site___Backend.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin/contact")]
+    public class ContactController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public ContactController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        #region List
+
+        [HttpGet("list", Name = "admin-contact-list")]
+        public async Task<IActionResult> ListAsync()
+
+        {
+            var model = await _dataContext.Contacts
+                .Select(a => new ListItemViewModel(a.Id, a.Name, a.Email, a.Message, a.Phone, a.CreatedAt, a.UpdatedAt ))
+                .ToListAsync();
+
+            return View(model);
+        }
+
+        #endregion
+    }
+}
---
+++ Pronia Final - Backend/Pronia Site - Backend/Areas/Admin/ViewModels/Contact/ListItemViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Pronia_Site___Backend.Areas.Admin.ViewModels.Contact
+{
+    public class ListItemViewModel
+    {
+        public ListItemViewModel(int id, string? name, string? email, string? phone, string? message, DateTime createdAt, DateTime updatedAt)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Phone = phone;
+            Message = message;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+        }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Message { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
