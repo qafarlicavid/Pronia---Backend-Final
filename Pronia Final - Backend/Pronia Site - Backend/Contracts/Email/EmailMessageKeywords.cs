74c8c52c3f847bc0c4f90382e41b79c65a7c6d48
qafarlicavid <javidig@code.edu.az>
2023-02-02 16:25:21 +0400

    Email Conteacts created

---
+++ Pronia Final - Backend/Pronia Site - Backend/Contracts/Email/EmailMessageKeywords.cs
@@ -0,0 +1,7 @@
+﻿namespace Pronia_Site___Backend.Contracts.Email
+{
+    public static class EmailMessageKeywords
+    {
+        public const string ACTIVATION_URL = "{activation_url}";
+    }
+}
---
+++ Pronia Final - Backend/Pronia Site - Backend/Contracts/Email/EmailMessages.cs
@@ -0,0 +1,16 @@
+﻿namespace Pronia_Site___Backend.Contracts.Email
+{
+    public static class EmailMessages
+    {
+        public static class Subject
+        {
+            public const string ACTIVATION_MESSAGE = $"Hesabin aktivlesdirilmesi";
+            public const string NOTIFICATION_MESSAGE = $"Orderinizin Statusu Yenilendi Xais Edirik Yoxlayin";
+        }
+
+        public static class Body
+        {
+            public const string ACTIVATION_MESSAGE = $"Sizin activation urliniz : {EmailMessageKeywords.ACTIVATION_URL}";
+        }
+    }
+}
