f220d9daee12a3b2511295b700b61be455fbbacc
qafarlicavid <javidig@code.edu.az>
2023-02-02 16:24:15 +0400

    UserActivation Service created

---
+++ Pronia Final - Backend/Pronia Site - Backend/Services/Abstracts/IUserActivationService.cs
@@ -0,0 +1,9 @@
+﻿using Pronia_Site___Backend.Database.Models;
+
+namespace Pronia_Site___Backend.Services.Abstracts
+{
+    public interface IUserActivationService
+    {
+        Task SendActivationUrlAsync(User user);
+    }
+}
