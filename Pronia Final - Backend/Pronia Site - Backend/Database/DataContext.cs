bb5e64eacfd11ded11e3e8fb4b5cc41ff67177ed
qafarlicavid <javidig@code.edu.az>
2023-02-02 15:18:43 +0400

    DataContext created

---
+++ Pronia Final - Backend/Pronia Site - Backend/Database/DataContext.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pronia_Site___Backend.Database.Models;
+using Pronia_Site___Backend.Database.Models.Common;
+using Pronia_Site___Backend.Extensions;
+
+namespace Pronia_Site___Backend.Database
+{
+    public partial class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserActivation> UserActivations { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketProduct> BasketProducts { get; set; }
+        public DbSet<Navbar> Navbars { get; set; }
+        public DbSet<SubNavbar> SubNavbars { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Size> Sizes { get; set; }
+        public DbSet<ProductColor> ProductColors { get; set; }
+        public DbSet<ProductSize> ProductSizes { get; set; }
+        public DbSet<PaymentBenefit> PaymentBenefits { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<BlogTag> BlogTags { get; set; }
+        public DbSet<BlogImage> BlogImages { get; set; }
+        public DbSet<BlogVideo> BlogVideos { get; set; }
+        public DbSet<ProductTag> ProductTags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly<Program>();
+        }
+
+    }
+
+    public partial class DataContext
+    {
+        public override int SaveChanges()
+        {
+            AutoAudit();
+
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            AutoAudit();
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            AutoAudit();
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            AutoAudit();
+
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        private void AutoAudit()
+        {
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                if (entry.Entity is not IAuditable auditableEntry) //Short version
+                {
+                    continue;
+                }
+
+                //IAuditable auditableEntry = (IAuditable)entry;
+
+                DateTime currentDate = DateTime.Now;
+
+                if (entry.State == EntityState.Added)
+                {
+                    auditableEntry.CreatedAt = currentDate;
+                    auditableEntry.UpdatedAt = currentDate;
+                }
+                else if (entry.State == EntityState.Modified)
+                {
+                    auditableEntry.UpdatedAt = currentDate;
+                }
+            }
+        }
+    }
+}
